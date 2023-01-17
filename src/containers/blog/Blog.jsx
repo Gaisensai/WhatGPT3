53dc4dac7ff3ff6b06bdcf0099139f953f535b67
dunsin <ajibabidunsin@gmail.com>
2023-01-17 22:31:39 +0100

    blog section completed

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -18,6 +18,7 @@ const Blog = () => {
           <Article imgUrl={blog02} date="Sep 26, 2021" title="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
           <Article imgUrl={blog03} date="Sep 26, 2021" title="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
           <Article imgUrl={blog04} date="Sep 26, 2021" title="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+          <Article imgUrl={blog05} date="Sep 26, 2021" title="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
         </div>
       </div>
     </div>
